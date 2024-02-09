aab5f238dd8d4b7a68c2ad42b2f6f32381697f09
khurram-uworx <khurram.aziz@uworx.co.uk>
2024-02-10 00:17:00 +0500

    1 telemetry is added in service and mvc

--- JiraWorkLogsService/Program.cs
+++ JiraWorkLogsService/Program.cs
@@ -1,11 +1,37 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Utils;
+
 namespace JiraWorkLogsService
 {
     public class Program
     {
+        internal static ActivitySource JiraActivitySource;
+        internal static Counter<int> CountGreetings;
+
         public static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
             builder.Services.AddHostedService<Worker>();
+
+            JiraActivitySource = new ActivitySource(builder.Environment.ApplicationName);
+            var greeterMeter = new Meter(builder.Environment.ApplicationName + ".Greeter");
+            CountGreetings = greeterMeter.CreateCounter<int>("greetings.count", description: "Counts the number of greetings");
+
+            var telemetryBuilder = builder.Services.AddOpenTelemetry();
+            telemetryBuilder.ConfigureResource(b => b.AddService(builder.Environment.ApplicationName));
+            telemetryBuilder.WithMetrics(metrics => metrics
+                .AddMeter(greeterMeter.Name));
+            telemetryBuilder.WithTracing(tracing => tracing
+                .AddHttpClientInstrumentation()
+                .AddSource(JiraActivitySource.Name)
+                .AddZipkinExporter(b => b.Endpoint = Constants.ZipkinEndpoint)
+                .AddConsoleExporter());
+            // for otlp
+            //tracing.AddOtlpExporter(otlpOptions => otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint));
 
             var host = builder.Build();
             host.Run();
--- JiraWorkLogsService/Worker.cs
+++ JiraWorkLogsService/Worker.cs
@@ -2,22 +2,32 @@ namespace JiraWorkLogsService
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+        private readonly ILogger<Worker> logger;
 
         public Worker(ILogger<Worker> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var random = new Random();
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
-                {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                }
-                await Task.Delay(1000, stoppingToken);
+                var activityName = $"Tick";
+                using var activity = Program.JiraActivitySource.StartActivity(activityName);
+
+                await Task.Delay(random.Next(2000));
+
+                if (logger.IsEnabled(LogLevel.Information))
+                    this.logger.LogInformation("Incrementing greeting at: {time}", DateTimeOffset.Now);
+
+                Program.CountGreetings.Add(1);
+
+                activity?.SetTag("greeting", Program.CountGreetings);
+
+                await Task.Delay(3000, stoppingToken);
             }
         }
     }
