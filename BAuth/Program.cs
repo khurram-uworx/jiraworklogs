541d82eae1a4e9a074ea9b2bd22adf04c321da48
khurram-uworx <khurram.aziz@uworx.co.uk>
2024-02-10 20:26:10 +0500

    9 nginx

--- BAuth/Program.cs
+++ BAuth/Program.cs
@@ -1,3 +1,6 @@
+using Formix.Authentication.Basic;
+using System.Security.Claims;
+
 namespace BAuth
 {
     public class Program
@@ -6,6 +9,21 @@ namespace BAuth
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
+
+            app.UseBasicAuthentication(creds =>
+            {
+                bool ok = false;
+
+                ok = (null != creds && "uworx" == creds.Password && "khurram" == creds.UserName);
+
+                if (ok)
+                    return new ClaimsPrincipal(new[]
+                    {
+                        new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, creds.UserName) }, "uworx-auth-type")
+                    });
+                else
+                    return null;
+            }, "uworx-realm", 3000);
 
             app.MapGet("/", () => "Hello World!");
 
