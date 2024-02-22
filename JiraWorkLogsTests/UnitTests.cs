fd04fb22ed12984198e73416058eacfbdd48e6e0
khurram-uworx <khurram.aziz@uworx.co.uk>
2024-02-22 21:47:30 +0500

    22-unit tests

---
+++ JiraWorkLogsTests/UnitTests.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using Utils;
+
+namespace JiraWorkLogsTests;
+
+[TestClass]
+public class UnitTests
+{
+    [TestMethod]
+    public void TestConstants()
+    {
+        // Arrange
+
+        // Act
+        var connectionString = Constants.DatabaseConnectionString;
+
+        // Assert
+        Assert.IsTrue(null != connectionString && connectionString.Length > 0);
+
+        connectionString.Should().NotBeNullOrEmpty();
+        connectionString.Should().Contain("Server");
+        connectionString.Should().Contain("Database");
+        connectionString.Should().Contain("UserName");
+        connectionString.Should().Contain("Password");
+
+        connectionString.Should()
+            .NotBeNullOrEmpty()
+            .And.ContainAll(["Server", "Database", "UserName", "Password"],
+                "because it should contain all keywords");
+    }
+}
